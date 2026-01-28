8a2fccfaaaf2431b89870c4b30e944f86e232c3f
Sebastiamg <97122601+Sebastiamg@users.noreply.github.com>
2026-01-28 17:26:14 -0500

    132234

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/VerbsGame/',
+  // base: '/VerbsGame/',
   plugins: [
     tailwindcss(),
     react({
